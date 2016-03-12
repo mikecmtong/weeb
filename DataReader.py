b0ceb2563b81bc31a247343c3dca80983a1c7ff6
Michael Christopher Tong <mct2159@columbia.edu>
2016-03-12 13:24:37 -0500

    read() filter for geo

--- DataReader.py
+++ DataReader.py
@@ -1,4 +1,4 @@
-__author__ == 'michaeltong'
+__author__ = 'michaeltong'
 
 import os.path
 import pandas
@@ -23,6 +23,43 @@ class GeneralReader():
     	:rtype: None
     	'''
 
-    	if not os.path.exists(GNRL_CSV_PATH_2013)
-        self.cms_lookup = pandas.read_csv(CSV_PATH, quotechar='"', quoting=csv.QUOTE_ALL)
+    	if not os.path.exists(GNRL_CSV_PATH_2013):
+    		raise FileNotFoundError('The .csv file was not found in %s' % GNRL_CSV_PATH_2013)
+    	else:
+        	self.cms_lookup = pandas.read_csv(CSV_PATH, quotechar='"', quoting=csv.QUOTE_ALL, usecols=categories)
 
+    def read(self, geo, params):
+    	'''
+    	Queries the cms dataframe
+    	:param geo: geographic filters, possibilities are city, state, zipcode. Use * for any.
+    	e.g. geo = {'city': 'Cleveland', 'state': 'OH', 'zipcode': '*'}
+    	:type geo: dict
+    	:param params: other filters. Not supported yet
+    	:type params: dict
+    	'''
+
+        search = ''
+
+    	if 'city' in geo:
+    		geo['city'] = string.upper(geo['city'])
+    	else:
+    		geo['city'] = '*'
+
+    	if 'state' not in geo:
+    		geo['state'] = '*'
+
+
+    	if 'zipcode' in geo and geo['zipcode'] != '*':
+    		geo['zipcode'] = geo['zipcode'][0:5]   #set 5 letter zipcode
+    	else:
+    		geo['zipcode'] = '*'
+
+    	for param in geo.keys():
+    		if geo[param] != '*':
+    			search += "%s == '%s' " %(param, geo[param])
+    			search += 'and '
+
+    	if len(search) > 0:
+    		return df.query(search[-4])
+    	else:
+    		return df
